d1ee3eb6ef63c689643e8d52dace9e81c1f2edcc
mrobison-nd <mrobison@nd.edu>
2025-10-02 14:39:28 -0400

    Add files via upload

--- ChangeDetection.js
+++ ChangeDetection.js
@@ -2078,6 +2078,38 @@ function end_expRoutineBegin(snapshot) {
     continueRoutine = true; // until we're told otherwise
     end_expMaxDurationReached = false;
     // update component parameters for each repeat
+    // Disable downloading results to browser
+    psychoJS._saveResults = 0; 
+    
+    // Generate filename for results
+    let filename = psychoJS._experiment._experimentName + '_' + psychoJS._experiment._datetime + '.csv';
+    
+    // Extract data object from experiment
+    let dataObj = psychoJS._experiment._trialsData;
+    
+    // Convert data object to CSV
+    let data = [Object.keys(dataObj[0])].concat(dataObj).map(it => {
+        return Object.values(it).toString()
+    }).join('\n')
+    
+    // Send data to OSF via DataPipe
+    console.log('Saving data...');
+    fetch('https://pipe.jspsych.org/api/data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: '*/*',
+        },
+        body: JSON.stringify({
+            experimentID: 'W1HIqKoOUEoW', // ⭑ UPDATE WITH YOUR DATAPIPE EXPERIMENT ID ⭑
+            filename: filename,
+            data: data,
+        }),
+    }).then(response => response.json()).then(data => {
+        // Log response and force experiment end
+        console.log(data);
+        quitPsychoJS();
+    })
     psychoJS.experiment.addData('end_exp.started', globalClock.getTime());
     end_expMaxDuration = null
     // keep track of which components have finished
